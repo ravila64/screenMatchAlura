3f11ba1ec21e1d4f83006bb3ec7be9e2d4d2010b
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-04-07 16:08:06 -0500

    terminacion cap 1, hasta persona y producto

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,12 +1,10 @@
 package model;
-
 public class Persona {
     public String nombre;
     public int edad;
     public void cumplirAnios(){
         edad++;
     }
-
     @Override
     public String toString() {
         return "Persona{" +
